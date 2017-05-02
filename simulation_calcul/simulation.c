98df3135d6ac651e3ea48bd7eadabe8966cc7695
PaulMorgon <Milloser@live.fr>
2017-05-02 13:03:08 +0200

    Ajout d'un fichier d'en tête

--- simulation_calcul/simulation.c
+++ simulation_calcul/simulation.c
@@ -1,79 +1,13 @@
+#include "./simulation.h"
 #include<stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
-#define NOMBRE_TIC 10		//Le nombre de TIC à effectuer.
-#define NOMBRE_SLOT 10		//Le nombre de slot dans l'anneau.
-#define NOMBRE_NOEUD 5	//Le nombre de noeud dans l'anneau.
-
 /* Déclaration des structures */
 
-/**
- * Structure représentant un groupe de message arrivant dans le noeud.
- * Un groupe de message à une taille ainsi qu'un TIC d'arrivé dans le noeud.
-*/
-struct Messages
-{
-	int TIC_arrive;
-	int taille;
-};
-typedef struct Messages Messages;
-
-/**
- * Structure représentant un paquet de message curculant dans les slots.
- * Cette structure contient un indice du noeud emetteur du paquet, un nombre de message, et un tableau des différents messages.
-*/
-struct PaquetMessage
-{
-	int indice_noeud_emetteur;
-	int nombre_messages;
-	Messages messages[80];		//Un slot ne peut contenir plus de 80 messages
-};
-typedef struct PaquetMessage PaquetMessage;
-
-/**
- * Structure représentant un Noeud de l'anneau.
- * Elle contient un nombre de message, les indices des slots avec lesquels le noeud peut intéragir, ainsi que le nombre d'antenne liée au noeud et le décalage avec laquelle le noeud doit recevoir un message de ses antennes.
-*/
-
-struct Noeud
-{
-	int id;
-	int nb_message;
-	int indice_slot_lecture;
-	int indice_slot_ecriture;
-	int nb_antenne;		//Indique le nombre d'antenne auquel est lié le noeuds
-	int debut_periode;		//Le décalage selon lequel le noeud recoit des messages des antennes
-};
-typedef struct Noeud Noeud;
-
-/**
- * Structure représentant un slot dans l'anneau.
- * Un Slot est caractérisé par un id (unique), un booléen insiquant si le slot contient un message ou non, ainsi que les indices du noeud lié au slot s'il y en a un.
-*/
-
-struct Slot
-{
-	int id;
-	int paquet_message;		//Indique si le slot possede un paquet de message
-	int indice_noeud_lecture;		//Si le slot ne peut accèder a aucun noeud, ce champs vaut -1
-	int indice_noeud_ecriture;		//Si le slot ne peut accèder a aucun noeud, ce champs vaut -1
-};
-typedef struct Slot Slot;
-
 /* Déclaration des fonctions */
-void afficher_slots( Slot slots[] );
-void initialiser_slots( Slot slots[] );
-
-void afficher_noeuds( Noeud noeuds[] );
-void initialiser_noeuds( Noeud noeuds[], Slot slots[] );
 
 /*	 M A I N 	 */
-
-/**
- * Méthode main.
- * Elle effectue un nombre prédéfini de TIC dans l'anneau et effectue des calculs sur le déroulement des actions dans l'anneau.
-*/
 int main ()
 {
 	int nombre_tic_restant = NOMBRE_TIC;
@@ -98,9 +32,6 @@ int main ()
 	return 0;
 }
 
-/**
- * Initialise les slots de l'anneau.
-*/
 void initialiser_slots( Slot slots[] )
 {
 	int cpt = 0;
@@ -111,9 +42,6 @@ void initialiser_slots( Slot slots[] )
 	}
 }
 
-/**
- * Affiche les slots de l'anneau.
-*/
 void afficher_slots( Slot slots[] )
 {
 	int i;
@@ -124,9 +52,6 @@ void afficher_slots( Slot slots[] )
 	}
 }
 
-/**
- * Initialise les noeuds de l'anneau et met à jour les slots.
-*/
 void initialiser_noeuds( Noeud noeuds[], Slot slots[] )
 {
 	time_t t;
@@ -158,9 +83,6 @@ void initialiser_noeuds( Noeud noeuds[], Slot slots[] )
 	}
 }
 
-/**
- * Affiche les noeuds de l'anneau.
-*/
 void afficher_noeuds( Noeud noeuds[] )
 {
 	printf("Les neouds de l'anneau\n");
